4c7ca1033cc506fe59cf65f404baed728db4780f
GabrielHidro <gabrielhidro.dev@gmail.com>
2023-04-26 11:03:31 -0300

    Instalando e configurando carrossel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,21 @@ import Camisa1 from "../assets/camisetas/Variant6.png"
 import Camisa2 from "../assets/camisetas/Variant8.png"
 import Camisa3 from "../assets/camisetas/Variant7.png"
 
+import {useKeenSlider} from "keen-slider/react"
+import 'keen-slider/keen-slider.min.css'
+
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 2.5,
+      spacing: 48,
+    }
+  })
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={Camisa1} alt="Produto" width={520} height={480} />
 
         <footer>
@@ -17,8 +27,26 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={Camisa2} alt="Produto" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={Camisa3} alt="Produto" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={Camisa3} alt="Produto" width={520} height={480} />
 
         <footer>
           <strong>Camiseta x</strong>
